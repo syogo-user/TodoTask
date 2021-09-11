43629c26aa2b0e183e0c7331de46aaf410f55ee7
syogo-user <s.photo.trumpet.005@gmail.com>
2021-09-11 17:12:31 +0900

    feat: タスクの登録,編集,削除 機能の作成

--- app/src/main/java/com/example/todotask/MainActivity.kt
+++ app/src/main/java/com/example/todotask/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.todotask
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -11,6 +13,8 @@ import io.realm.Sort
 
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
+const val EXTRA_TASK = "com.example.todotask.TASK"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mRealm: Realm
@@ -26,8 +30,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            startActivity(intent)
         }
         // Realmの設定
         mRealm = Realm.getDefaultInstance()
@@ -37,15 +41,31 @@ class MainActivity : AppCompatActivity() {
         mTaskAdapter = TaskAdapter(this@MainActivity)
         listView1.setOnItemClickListener { parent, view, position, id ->
             // listViewをタップ時
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
 
         listView1.setOnItemLongClickListener{ parent, view, postion, id ->
-            // listViewを長押
+            // listViewを長押し
+            // タスク削除
+            val task = parent.adapter.getItem(postion) as Task
+            val alert = AlertDialog.Builder(this@MainActivity)
+            alert.setTitle("削除")
+            alert.setMessage(task.title + "を削除しますか？")
+            alert.setPositiveButton("OK") { _, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+                reloadListView()
+            }
+            alert.setNegativeButton("CANCEL", null)
+            val dialog = alert.create()
+            dialog.show()
             true
         }
-
-        // タスクの追加
-        addTask()
 
         reloadListView()
     }
@@ -63,16 +83,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mRealm.close()
-    }
-
-    private fun addTask() {
-        val task = Task()
-        task.title = "タイトル1"
-        task.contents = "内容1"
-        task.date = Date()
-        task.id = 0
-        mRealm.beginTransaction()
-        mRealm.copyToRealmOrUpdate(task)
-        mRealm.commitTransaction()
     }
 }
